9394d3021bc0007fcc8fac976d43e20abcf098e1
arisonreis <arisontechp@gmail.com>
2023-11-23 18:40:11 -0300

    feat: add tcp server and handling with clients messages

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
-fn main() {
-    println!("Hello, world!");
+use log::{info, trace};
+use simple_logger;
+use std::{
+    io::{BufRead, BufReader, Write},
+    net::TcpListener,
+};
+use tokio;
+
+#[tokio::main]
+async fn main() {
+    simple_logger::init().unwrap();
+
+    let port: &str = "8080";
+    let host: &str = "localhost:";
+    let server_addr = format!("{}{}", &host, &port);
+
+    let listener = TcpListener::bind(&server_addr).unwrap();
+    info!("Server is Running on port : {}", &port);
+
+    loop {
+        trace!("Awaiting connection...");
+        let (mut socket, addr) = listener.accept().unwrap();
+        info!("client connected :{}", addr);
+
+        tokio::spawn(async move {
+            loop {
+                let mut reader = BufReader::new(&mut socket);
+                let mut line: String = String::new();
+
+                let bytes_read: usize = reader.read_line(&mut line).unwrap();
+
+                if bytes_read == 0 {
+                    break;
+                }
+                let _ = socket
+                    .write_all(&mut line.as_bytes())
+                    .expect("failed to write data to socket");
+
+                let _ = socket.flush().expect("failed to flush socket");
+                info!("client {} sent message : {:?}", addr, line.trim_end());
+                line.clear();
+            }
+        });
+    }
 }
